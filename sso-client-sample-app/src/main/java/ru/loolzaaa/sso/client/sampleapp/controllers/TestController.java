5893ccde50433c3773d1ae360a3f4213d693c9ba
Korsakov A.N. (20211) <loolzaaa@gmail.com>
2022-04-08 09:08:52 +0500

    Add user of UserService to TestController, update template

--- sso-client-sample-app/src/main/java/ru/loolzaaa/sso/client/sampleapp/controllers/TestController.java
+++ sso-client-sample-app/src/main/java/ru/loolzaaa/sso/client/sampleapp/controllers/TestController.java
@@ -1,15 +1,21 @@
 package ru.loolzaaa.sso.client.sampleapp.controllers;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import ru.loolzaaa.sso.client.core.UserService;
+import ru.loolzaaa.sso.client.core.model.User;
 
 import java.util.Random;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class TestController {
+
+    private final UserService userService;
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/get/test/{id}")
@@ -26,6 +32,15 @@ public class TestController {
         Test test = new Test();
         test.setName("Name_" + id);
         test.setValue(new Random().nextInt());
+        return test;
+    }
+
+    @GetMapping("/get/test")
+    public Test testUser() {
+        User requestUser = userService.getRequestUser();
+        Test test = new Test();
+        test.setName(requestUser.getLogin());
+        test.setValue(requestUser.getId().intValue());
         return test;
     }
 
