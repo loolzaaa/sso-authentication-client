6b1140bf926b8832109a6e97751c7c8864565b42
loolzaaa <loolzaaa@gmail.com>
2022-09-25 13:07:12 +0500

    Make auto configuration before spring security

--- sso-client-spring-boot-autoconfigure/src/main/java/ru/loolzaaa/sso/client/autoconfigure/SsoClientAutoConfiguration.java
+++ sso-client-spring-boot-autoconfigure/src/main/java/ru/loolzaaa/sso/client/autoconfigure/SsoClientAutoConfiguration.java
@@ -3,7 +3,7 @@ package ru.loolzaaa.sso.client.autoconfigure;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
@@ -35,7 +35,7 @@ import java.time.Duration;
 @EnableConfigurationProperties(SsoClientProperties.class)
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @ConditionalOnProperty(prefix = "sso.client", value = { "applicationName", "entryPointAddress", "entryPointUri" })
-@AutoConfigureAfter(SecurityAutoConfiguration.class)
+@AutoConfigureBefore(SecurityAutoConfiguration.class)
 @Import({ SsoClientHttpConfiguration.class, SsoClientFilterConfiguration.class, SsoClientEndpointConfiguration.class })
 public class SsoClientAutoConfiguration {
 
