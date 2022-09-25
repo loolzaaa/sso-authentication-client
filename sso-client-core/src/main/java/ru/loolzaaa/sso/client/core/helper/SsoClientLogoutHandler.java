3d8613c4f230dd0e0380c0f1bd6518bef3eac26a
loolzaaa <loolzaaa@gmail.com>
2022-09-25 13:04:57 +0500

    Add interface for custom logout handlers

---
+++ sso-client-core/src/main/java/ru/loolzaaa/sso/client/core/helper/SsoClientLogoutHandler.java
@@ -0,0 +1,6 @@
+package ru.loolzaaa.sso.client.core.helper;
+
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+public interface SsoClientLogoutHandler extends LogoutHandler {
+}
