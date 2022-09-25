f0e86514ccc34e411dae80e58047349d148b3a5a
loolzaaa <loolzaaa@gmail.com>
2022-09-25 13:07:33 +0500

    Cleanup SecurityConfig

--- sso-client-sample-app/src/main/java/ru/loolzaaa/sso/client/sampleapp/config/SecurityConfig.java
+++ sso-client-sample-app/src/main/java/ru/loolzaaa/sso/client/sampleapp/config/SecurityConfig.java
@@ -1,40 +1,24 @@
 package ru.loolzaaa.sso.client.sampleapp.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-import ru.loolzaaa.sso.client.autoconfigure.SsoClientHttpConfigurer;
 import ru.loolzaaa.sso.client.core.helper.SsoClientApplicationRegister;
+import ru.loolzaaa.sso.client.core.helper.SsoClientLogoutHandler;
 import ru.loolzaaa.sso.client.core.model.UserPrincipal;
 
-@RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final SsoClientHttpConfigurer ssoClientHttpConfigurer;
-
-    private final CustomLogoutHandler customLogoutHandler;
+public class SecurityConfig implements WebSecurityCustomizer {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .apply(ssoClientHttpConfigurer)
-                .and()
-                .logout()
-                    .addLogoutHandler(customLogoutHandler);
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web
-                .ignoring()
-                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    public void customize(WebSecurity web) {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Component
@@ -42,6 +26,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         public void register(UserPrincipal userPrincipal) {
             System.out.println("---------   Hello from application register hook!   ---------");
+            ///////////////////////////////////////////////
+            //
+            // Application-specific actions for register ...
+            //
+            ///////////////////////////////////////////////
+        }
+    }
+
+    @Component
+    public class CustomLogoutHandler implements SsoClientLogoutHandler {
+        @Override
+        public void logout(HttpServletRequest req, HttpServletResponse resp, Authentication auth) {
+            System.out.println("---------   Hello from custom logout handler!   ---------");
+            ///////////////////////////////////////////////
+            //
+            // Application-specific logout ...
+            //
+            ///////////////////////////////////////////////
         }
     }
 }
