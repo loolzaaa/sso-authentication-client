c93fce196b14e4ed8b4f4a9f15f664786fa0fcab
loolzaaa <loolzaaa@gmail.com>
2023-01-07 00:50:43 +0500

    Implement sso client configurer

--- sso-client-sample-app/src/main/java/ru/loolzaaa/sso/client/sampleapp/config/SecurityConfig.java
+++ sso-client-sample-app/src/main/java/ru/loolzaaa/sso/client/sampleapp/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package ru.loolzaaa.sso.client.sampleapp.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -10,36 +9,40 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import ru.loolzaaa.sso.client.core.application.SsoClientApplicationRegister;
 import ru.loolzaaa.sso.client.core.application.SsoClientLogoutHandler;
+import ru.loolzaaa.sso.client.core.config.SsoClientConfigurer;
 import ru.loolzaaa.sso.client.core.model.UserPrincipal;
 import ru.loolzaaa.sso.client.core.security.matcher.BasicAuthenticationConfigurer;
-import ru.loolzaaa.sso.client.core.security.matcher.BasicAuthenticationRegistry;
 import ru.loolzaaa.sso.client.core.security.matcher.PermitAllMatcherRegistry;
+import ru.loolzaaa.sso.client.core.security.matcher.WebhookHandlerRegistry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Set;
 
 @Configuration
-public class SecurityConfig implements WebSecurityCustomizer {
+public class SecurityConfig implements WebSecurityCustomizer, SsoClientConfigurer {
 
     @Override
     public void customize(WebSecurity web) {
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
-    @Bean
-    PermitAllMatcherRegistry permitAllMatcherRegistry() {
-        PermitAllMatcherRegistry permitAllMatcherRegistry = new PermitAllMatcherRegistry();
-        permitAllMatcherRegistry.addPermitAllMatcher(HttpMethod.GET, true, "/api/time");
-        return permitAllMatcherRegistry;
+    @Override
+    public void addPermitAllMatcher(PermitAllMatcherRegistry registry) {
+        registry.addPermitAllMatcher(HttpMethod.GET, true, "/api/time");
     }
 
-    @Bean
-    BasicAuthenticationRegistry basicAuthenticationRegistry(BasicAuthenticationConfigurer basicAuthenticationBuilder) {
-        return basicAuthenticationBuilder
+    @Override
+    public void configureBasicAuthentication(BasicAuthenticationConfigurer configurer) {
+        configurer
                 .addUser("test", "test", Set.of("test"))
-                .addRequestMatcher("/api/get/basic2/**", new String[]{"test"})
-                .build();
+                .addRequestMatcher("/api/get/basic2/**", new String[]{"test"});
+        SsoClientConfigurer.super.configureBasicAuthentication(configurer);
+    }
+
+    @Override
+    public void addWebhooks(WebhookHandlerRegistry registry) {
+        registry.addWebhook("WEBHOOK_VIA_CONFIG", "PASSWORD"::equals, System.err::println);
     }
 
     @Component
@@ -56,7 +59,7 @@ public class SecurityConfig implements WebSecurityCustomizer {
     }
 
     @Component
-    public class CustomLogoutHandler implements SsoClientLogoutHandler {
+    static class CustomLogoutHandler implements SsoClientLogoutHandler {
         @Override
         public void logout(HttpServletRequest req, HttpServletResponse resp, Authentication auth) {
             System.out.println("---------   Hello from custom logout handler!   ---------");
